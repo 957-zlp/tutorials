11c0884021c7b76d88e8d94717f3936c2a19ef0d
957-zlp <59875884+957-zlp@users.noreply.github.com>
2020-02-04 17:17:40 +0800

    Update plt13_image.py

--- matplotlibTUT/plt13_image.py
+++ matplotlibTUT/plt13_image.py
@@ -18,13 +18,13 @@ a = np.array([0.313660827978, 0.365348418405, 0.423733120134,
               0.423733120134, 0.525083754405, 0.651536351379]).reshape(3,3)
 
 """
-for the value of "interpolation", check this:
+for the value of "interpolation", check this:  不同的图像格式
 http://matplotlib.org/examples/images_contours_and_fields/interpolation_methods.html
 for the value of "origin"= ['upper', 'lower'], check this:
 http://matplotlib.org/examples/pylab_examples/image_origin.html
 """
 plt.imshow(a, interpolation='nearest', cmap='bone', origin='lower')
-plt.colorbar(shrink=.92)
+plt.colorbar(shrink=.92)  压缩到92%
 
 plt.xticks(())
 plt.yticks(())
